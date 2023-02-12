0088998ed042c6cd0f99e1ecdbd506aab88f56ce
Auyen-KM <auenkm@gmail.com>
2023-02-12 08:02:50 +0600

    little changes in main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import customtkinter as ct
 
+ct.set_appearance_mode("dark")
+ct.set_default_color_theme("green")
+
 
 class App(ct.CTk):
     def __init__(self):
         super().__init__()
         self.title("Tic Tac Toe")
-        self.geometry("500x400+800+300")
+        self.geometry("500x550+600+100")
+        self.frame = ct.CTkFrame(master=self, width=400, height=400)
+        self.frame.pack(pady=30, padx=40)
+        self.field_button = ct.CTkButton(master=self.frame)
+        self.field_button.pack()
 
 
 if __name__ == "__main__":
